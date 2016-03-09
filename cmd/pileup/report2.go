b28a6e9f4c23e9e6b9bca07dab713d6cb9bd6f46
Mingzhi Lin <mingzhi9@gmail.com>
2016-03-09 12:31:04 -0500

    output depth and mean pi in report2

--- cmd/pileup/report2.go
+++ cmd/pileup/report2.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"github.com/bmatsuo/lmdb-go/lmdb"
+	"github.com/montanaflynn/stats"
 	"gopkg.in/vmihailenco/msgpack.v2"
 	"log"
 	"os"
+	"sort"
 )
 
 type cmdReport2 struct {
@@ -16,12 +18,17 @@ type cmdReport2 struct {
 
 func (c *cmdReport2) run() {
 	geneSnpChan := getAllSNPs(c.resultsDB)
-	geneResChan := c.getFeatures(geneSnpChan)
-	c.groupGr(geneResChan)
+	c.getFeatures(geneSnpChan)
 }
 
-func (c *cmdReport2) getFeatures(geneSnpChan chan SNPArr) chan GeneRes {
-	ch := make(chan GeneRes)
+func (c *cmdReport2) getFeatures(geneSnpChan chan SNPArr) {
+	w, err := os.Create(c.prefix + ".detectable.gene.csv")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer w.Close()
+	w.WriteString("patric_id,genome,figfam,sample,pi,depth\n")
+
 	fn := func(txn *lmdb.Txn) error {
 		dbi, err := txn.OpenDBI("feature", 0)
 		if err != nil {
@@ -29,11 +36,11 @@ func (c *cmdReport2) getFeatures(geneSnpChan chan SNPArr) chan GeneRes {
 		}
 
 		for gs := range geneSnpChan {
-			k := gs.Key
-			values := []float64{}
-			for _, snp := range gs.Arr {
-				values = append(values, float64(len(snp.Bases)))
+			if len(gs.Arr) < 100 {
+				continue
 			}
+
+			k := gs.Key
 			v, err := txn.Get(dbi, k)
 			if err != nil {
 				return err
@@ -42,42 +49,40 @@ func (c *cmdReport2) getFeatures(geneSnpChan chan SNPArr) chan GeneRes {
 			if err := msgpack.Unmarshal(v, &f); err != nil {
 				return err
 			}
-			ch <- GeneRes{Key: k, Values: values, Feature: f}
+
+			seqLen := f.End - f.Start + 1
+
+			// calculate median of depth
+			depthArr := []float64{}
+			piArr := []float64{}
+			for _, snp := range gs.Arr {
+				pos := snp.Position - f.Start
+				if f.IsComplementaryStrand() {
+					pos = seqLen - 1 - pos
+				}
+				if (pos+1)%3 == 0 {
+					depthArr = append(depthArr, float64(len(snp.Bases)))
+					piArr = append(piArr, snp.Pi())
+				}
+
+			}
+			depthMedian, _ := stats.Median(depthArr)
+			sort.Float64s(piArr)
+			piMean, _ := stats.Mean(piArr[10 : len(piArr)-10])
+
+			w.WriteString(fmt.Sprintf("%s,%s,%s,%s,%g,%g\n",
+				f.PatricID,
+				f.TaxID,
+				f.FigfamID,
+				c.prefix,
+				piMean,
+				depthMedian))
 		}
 		return nil
 	}
-	go func() {
-		defer close(ch)
-		err := c.featureDB.View(fn)
-		if err != nil {
-			log.Panicln(err)
-		}
-	}()
 
-	return ch
-}
-
-func (c *cmdReport2) groupGr(grChan chan GeneRes) {
-	w, err := os.Create(c.prefix + ".detectable.gene.csv")
+	err = c.featureDB.View(fn)
 	if err != nil {
-		log.Fatalln(err)
-	}
-	defer w.Close()
-	w.WriteString("patric_id,genome,figfam,sample,depth\n")
-
-	for gr := range grChan {
-		values := gr.Values
-		total := 0.0
-		for _, v := range values {
-			total += v
-		}
-		m := total / float64(len(values))
-
-		w.WriteString(fmt.Sprintf("%s,%s,%s,%s,%g\n",
-			gr.Feature.PatricID,
-			gr.Feature.TaxID,
-			gr.Feature.FigfamID,
-			c.prefix,
-			m))
+		log.Panicln(err)
 	}
 }
